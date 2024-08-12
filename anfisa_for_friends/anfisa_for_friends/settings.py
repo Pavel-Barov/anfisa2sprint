9ae4319e3d6f6978cdcfaa737725a94ef571100b
PavelBarov <barovpaul@yandex.ru>
2024-08-12 15:20:48 +0500

    Final 8 sprint

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -11,8 +11,6 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
-    'homepage.apps.HomepageConfig',
-    'ice_cream.apps.IceCreamConfig',
     'about.apps.AboutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -20,6 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',    
+    'homepage.apps.HomepageConfig',
+    'ice_cream.apps.IceCreamConfig',
 ]
 
 MIDDLEWARE = [
@@ -30,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'anfisa_for_friends.urls'
@@ -79,7 +81,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -97,3 +99,7 @@ STATICFILES_DIRS = [
     ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
